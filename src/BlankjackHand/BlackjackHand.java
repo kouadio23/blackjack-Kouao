8573c13a860249b39da374b68ec2c20568d545bd
kouadio23 <83689222+kouadio23@users.noreply.github.com>
2022-11-09 01:44:34 -0700

    I am about to finish this great game

---
+++ src/BlankjackHand/BlackjackHand.java
@@ -0,0 +1,20 @@
+package BlankjackHand;
+
+import deck.Card;
+
+public class BlackjackHand {
+    public void addCard(Card dealCard) {
+    }
+
+    public int getBlackjackValue() {
+        return 0;
+    }
+
+    public String getCard(int i) {
+        return null;
+    }
+
+    public int getCardCount() {
+        return 0;
+    }
+}
---
+++ src/deck/Deck.java
@@ -0,0 +1,10 @@
+package deck;
+
+public class Deck {
+    public void shuffle() {
+    }
+
+    public Card dealCard() {
+        return null;
+    }
+}
